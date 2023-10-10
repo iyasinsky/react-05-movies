1b6d96cb134db691a3a155eeb7abcb05188b966d
Igor Yasinsky <iyasinskyy@gmail.com>
2023-10-10 23:30:43 +0300

    fixed basename

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
-import { lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import GlobalStyle from 'helpers/GlobalStyle';
 import Layout from './Layout';
-import NotFound from './NotFound';
+const NotFound = lazy(() => import('./NotFound'));
 const Home = lazy(() => import('pages/Home'));
 const Movies = lazy(() => import('pages/Movies'));
 const MovieDetails = lazy(() => import('pages/MovieDetails'));
@@ -12,17 +12,19 @@ const Reviews = lazy(() => import('pages/Reviews'));
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="movies" element={<Movies />} />
-          <Route path="movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
+      <Suspense fallback={<p>App loading...</p>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="movies" element={<Movies />} />
+            <Route path="movies/:movieId" element={<MovieDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
           </Route>
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
       <GlobalStyle />
     </>
   );
