c20d71d5ed6f4df921d9c8086cbd79a8ad4215bc
Igor Yasinsky <iyasinskyy@gmail.com>
2023-10-15 16:05:55 +0300

    fixing

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,8 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <p>Page not found</p>
-      <button type="button">Go back</button>
+      <button type="button" onClick={() => navigate(-1)}>
+        Go back
+      </button>
     </div>
   );
 };
